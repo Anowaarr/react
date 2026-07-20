e5955eb0ab30057c466108c6079449d2886db054
Anowaarr <itzanowar.jr@gmail.com>
2026-07-20 23:56:11 +0600

    Added files

--- myApp/src/About.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const About = () => <h1 style={{ textAlign: 'center', marginTop: '2rem' }}>About Us</h1>;
-
-export default About;
--- myApp/src/Services.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Services = () => <h1 style={{ textAlign: 'center', marginTop: '2rem' }}>Our Services</h1>;
-
-export default Services;
