c1cd5e09a72fed46c4919de7bee159eab148a7bd
Anowaarr <itzanowar.jr@gmail.com>
2026-07-28 00:20:54 +0600

    Added files

--- E-CommerceWeb/src/Router.js
+++ E-CommerceWeb/src/Router.js
@@ -1,2 +1,13 @@
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children: [
+      { index: true, Component: Home },
+      { path: "about", Component: About },
+    ],
+  },
+]);
