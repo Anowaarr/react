e302a7238d3f8259fbbca260cdd44a9eeafc0c79
Anowaarr <itzanowar.jr@gmail.com>
2026-08-03 23:45:50 +0600

    AddedFiles

--- E-CommerceWeb/vite.config.js
+++ E-CommerceWeb/vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 
-
+// git add .
 // git commit -m "AddedFiles"
+// git push
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
